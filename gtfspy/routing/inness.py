10f6c182251e53c86d5b8959a58628d80e90b211
javurena7 <javurena7@gmail.com>
2018-03-23 18:36:04 +0200

    Add distance to center

--- gtfspy/routing/inness.py
+++ gtfspy/routing/inness.py
@@ -1,5 +1,6 @@
 import datetime
 import geojson
+from geoindex import GeoGridIndex, GeoPoint
 from gtfspy.routing.journey_path_analyzer import NodeJourneyPathAnalyzer
 
 
@@ -7,6 +8,26 @@ class JourneyInness(NodeJourneyPathAnalyzer):
     def __init__(self, labels, walk_to_target_duration, start_time_dep, end_time_dep, origin_stop, gtfs):
         super().__init__(labels, walk_to_target_duration, start_time_dep, end_time_dep, origin_stop)
         self.gtfs = gtfs
+        self.radius = None
+        self.city_center = GeoPoint(60.171171, 24.941549)
+
+    def set_city_center(self, value):
+        assert type(value) is tuple, "City center must be tuple (lat, long)"
+        self.city_center = GeoPoint(value)
+
+    def distance_to_city_center(self):
+        geo_index = GeoGridIndex()
+        for lat, lon, ind in zip(self.gtfs.stops().lat, self.gtfs.stops().lon, self.gtfs.stops().stop_I):
+            geo_index.addPoint(GeoPoint(lat, lon, ref=ind)
+
+        dists = [(x[0].ref, x[1]) for geo_index.get_nearest_points(self.city_center, 30, 'km')]
+        return dists
+
+
+
+
+
+
 
 
 
