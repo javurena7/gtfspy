93f6427c3fd4100343f9e220e50f24e99d1c2ba6
javurena7 <javurena7@gmail.com>
2018-03-26 15:39:44 +0300

    Change distance function for local one

--- gtfspy/routing/inness.py
+++ gtfspy/routing/inness.py
@@ -1,46 +1,53 @@
-import datetime
 from pandas import cut
 from gtfspy.util import wgs84_distance
 from gtfspy.gtfs import GTFS
+
+# G = GTFS(path/to/sqlite)
+# I = Inness(G)
+# I.get_rings()
+# I.rings
 
 class Inness(object):
     def __init__(self, gtfs):
         self.gtfs = gtfs
         self.rings = None
-        self.city_center = GeoPoint(60.171171, 24.941549) #Rautatientori, Helsinki
+        self.city_center = (60.171171, 24.941549) #Rautatientori, Helsinki
         self.distance_to_city_center = None
 
     def set_city_center(self, value):
         assert type(value) is tuple, "City center must be tuple (lat, long)"
-        self.city_center = GeoPoint(value[0], value[1])
+        self.city_center = value
 
     def get_distance_to_city_center(self):
         """
-        Get distance from all the bus stops within a 30 km radius to the city center.
+        Get distance from all the stops to the city center.
         Output:
             (city_center): lists with [stop_I, distance_in_km] for each stop within a 30 km radius
         """
-        geo_index = GeoGridIndex(precision=3)
+        dists = []
+        latc, lonc = self.city_center
         for lat, lon, ind in zip(self.gtfs.stops().lat, self.gtfs.stops().lon, self.gtfs.stops().stop_I):
-            geo_index.add_point(GeoPoint(lat, lon, ref=ind))
-        dists = [[x[0].ref, x[1]] for x in geo_index.get_nearest_points(self.city_center, 30, 'km')]
+            dists.append([ind, wgs84_distance(lat, lon, latc, lonc)/1000.])
         self.distance_to_city_center = dists
 
-    def get_rings(self, number=90):
+    def get_rings(self, number=90, max_distance=30):
         """
         Get rings to calculate innes.
         Input:
             (number): number of equal length rings
+            (max_distance): maximum distance in km
         Output:
             (rings): dictionary with rings (keys) and lists with stop_I's within that ring
         """
         if not self.distance_to_city_center:
             self.get_distance_to_city_center()
-        dists = [x[1] for x in self.distance_to_city_center]
+        dists_id = [x for x in self.distance_to_city_center if x[1] < max_distance]
+        assert len(dists_id) > 0, "Reset city center - beyond any data point"
+        dists = [x[1] for x in dists_id]
         cuts = cut(dists, number, labels=False)
         self.cuts = cuts
         rings = {}
-        for stop, ring in zip(self.distance_to_city_center, cuts):
+        for stop, ring in zip(dists_ie, cuts):
             try:
                 rings[ring].append(stop[0])
             except:
