48faa17c933ff843f6d08a13038035cb02e92215
javurena7 <javurena7@gmail.com>
2018-03-26 14:29:12 +0300

    Change class structure / Add city-specific class

--- gtfspy/routing/inness_journey.py
+++ gtfspy/routing/inness_journey.py
@@ -1,10 +1,16 @@
 import datetime
+import geojson
 from pandas import cut
+from geoindex import GeoGridIndex, GeoPoint
+from gtfspy.routing.journey_path_analyzer import NodeJourneyPathAnalyzer
 from gtfspy.util import wgs84_distance
-from gtfspy.gtfs import GTFS
 
-class Inness(object):
-    def __init__(self, gtfs):
+# TODO: change this class to incorporate journey-specific stuff, this has moved to the inness object
+
+
+class JourneyInness(NodeJourneyPathAnalyzer):
+    def __init__(self, labels, walk_to_target_duration, start_time_dep, end_time_dep, origin_stop, gtfs):
+        super().__init__(labels, walk_to_target_duration, start_time_dep, end_time_dep, origin_stop)
         self.gtfs = gtfs
         self.rings = None
         self.city_center = GeoPoint(60.171171, 24.941549) #Rautatientori, Helsinki
@@ -46,5 +52,7 @@ class Inness(object):
             except:
                 rings[ring] = [stop[0]]
         self.rings = rings
+
+
 
 
