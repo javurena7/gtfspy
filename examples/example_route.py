75b0474affc11f3b7534d9713e23a0f4115997bd
javurena7 <javurena7@gmail.com>
2018-04-20 12:51:39 +0300

    Reset example_route file

--- examples/example_route.py
+++ examples/example_route.py
@@ -5,15 +5,13 @@ from gtfspy.route_types import ROUTE_TYPE_TO_COLOR
 
 from matplotlib import pyplot as plt
 from matplotlib import rc
-#import example_import
-from gtfspy.gtfs import GTFS
+import example_import
 
-G = GTFS('data/lm_daily.sqlite')
+G = example_import.load_or_import_example_gtfs()
 tz = G.get_timezone_pytz()
 
-"""
-from_stop_name = "Leikkimäki"
-to_stop_name = "Malminkartanontie"
+from_stop_name = "Ahkiotie 2 E"
+to_stop_name = "Kauppahalli P"
 from_stop_I = None
 to_stop_I = None
 stop_dict = G.stops().to_dict("index")
@@ -26,7 +24,9 @@ assert(from_stop_I is not None)
 assert(to_stop_I is not None)
 
 
-ROUTING_START_TIME_UT = G.get_suitable_date_for_daily_extract(ut=True) + 8 * 3600
+
+
+ROUTING_START_TIME_UT = G.get_suitable_date_for_daily_extract(ut=True) + 10 * 3600
 ROUTING_END_TIME_UT = G.get_suitable_date_for_daily_extract(ut=True) + 14 * 3600
 
 
@@ -34,17 +34,10 @@ connections = get_transit_connections(G, ROUTING_START_TIME_UT, ROUTING_END_TIME
 walk_network = get_walk_network(G)
 
 
-mpCSA = MultiObjectivePseudoCSAProfiler(connections,
-        targets=[to_stop_I], \
-        walk_network=walk_network, \
-        end_time_ut=ROUTING_END_TIME_UT, \
-        transfer_margin=120,\
-        start_time_ut=ROUTING_START_TIME_UT, \
-        walk_speed=1.5, \
-        verbose=True,\
-        track_vehicle_legs=True, \
-        track_time=True, \
-        track_route=True)
+mpCSA = MultiObjectivePseudoCSAProfiler(connections, targets=[to_stop_I], walk_network=walk_network,
+                                        end_time_ut=ROUTING_END_TIME_UT, transfer_margin=120,
+                                        start_time_ut=ROUTING_START_TIME_UT, walk_speed=1.5, verbose=True,
+                                        track_vehicle_legs=True, track_time=True, track_route=True)
 
 mpCSA.run()
 profiles = mpCSA.stop_profiles
@@ -57,61 +50,33 @@ walk_times = dict((key, value.get_walk_to_target_duration()) for (key, value) in
 nra = NodeJourneyPathAnalyzer(labels[from_stop_I], walk_times[from_stop_I], ROUTING_START_TIME_UT,
                               ROUTING_END_TIME_UT - CUTOFF_TIME, from_stop_I)
 
+fig = plt.figure()
+ax1 = fig.add_subplot(221)
 
 journey_path_letters, stop_letter_dict = nra.assign_path_letters(nra.journey_boarding_stops)
-
-nra.gtfs = G
-fig = plt.figure()
-#ax1 = fig.add_subplot(211)
-#ax2 = fig.add_subplot(222, projection="smopy_axes")
-ax2 = fig.add_subplot(111, projection="smopy_axes")
-"""
-"""
 nra.plot_fastest_temporal_distance_profile(ax=ax1,
                                            timezone=tz,
                                            plot_journeys=True,
                                            journey_letters=journey_path_letters,
                                            format_string="%H:%M:%S")
-"""
-import matplotlib.pyplot as plt
 
-def plot_journey(nra, G, to_stop_I, from_stop_I, stop_letter_dict):
-    fig = plt.figure()
-    ax2 = fig.add_subplot(111, projection="smopy_axes")
+nra.gtfs = G
+ax2 = fig.add_subplot(222, projection="smopy_axes")
 
-    for lats, lons, leg_type in nra.get_journey_trajectories():
-        ax2.plot(lons, lats, c=ROUTE_TYPE_TO_COLOR[leg_type], zorder=1)
+for lats, lons, leg_type in nra.get_journey_trajectories():
+    ax2.plot(lons, lats, c=ROUTE_TYPE_TO_COLOR[leg_type], zorder=1)
 
-    lat, lon = G.get_stop_coordinates(to_stop_I)
-    ax2.scatter(lon, lat, s=100, c="green", marker="X", zorder=2)
-    lat, lon = G.get_stop_coordinates(from_stop_I)
-    ax2.scatter(lon, lat, s=100, c="red", marker="X", zorder=2)
-    for stop_id, letters in stop_letter_dict.items():
-        lat, lon = G.get_stop_coordinates(stop_id)
-        im = ax2.scatter(lon, lat, s=20, c="grey", marker="o", zorder=2)
-        text = ax2.text(lon, lat, ",".join(letters), color="m", fontsize=10, va="top", ha="left", zorder=10)
+lat, lon = G.get_stop_coordinates(to_stop_I)
+ax2.scatter(lon, lat, s=100, c="green", marker="X", zorder=2)
+lat, lon = G.get_stop_coordinates(from_stop_I)
+ax2.scatter(lon, lat, s=100, c="red", marker="X", zorder=2)
+for stop_id, letters in stop_letter_dict.items():
+    lat, lon = G.get_stop_coordinates(stop_id)
+    ax2.scatter(lon, lat, s=20, c="grey", marker="o", zorder=2)
+    text = ax2.text(lon, lat, ",".join(letters), color="m", fontsize=10, va="top", ha="left", zorder=10)
 
-    fig.colorbar(im, ax=ax2)
-    return fig, im
+ax2.add_scale_bar()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
 ax3 = fig.add_subplot(223)
 diversity_dict = nra.get_simple_diversities()
 ax3.axis('tight')
@@ -127,4 +92,3 @@ ax4 = fig.add_subplot(224)
 ax4 = nra.plot_journey_graph(ax4)
 
 plt.show()
-"""
